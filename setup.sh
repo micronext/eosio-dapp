80fffb89bbbde48d8d999408319eb9a3d95a53b3
onegin 🚀 <evgeniyivanets@gmail.com>
2018-10-30 11:11:34 +0200

    dapps

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-if [ -d "dapp.init" ]; then
+if [ ! -d "./dapp.init" ]; then
     exit 1;
 fi
-docker pull eosio/eos-dev:v1.3.2
-docker pull mongo:4.0
-cd ./frontend && npm install && cd ../
-cd ./backend && npm install && cd ../
-echo " " > "dapp.init"
+
+docker pull eosio/eos:latest \
+    && docker pull eosio/eos-dev:latest \
+    && docker pull mongo:4 \
+    && docker pull node:8
+
+echo " " > ./dapp.init
+
+# cd ./frontend && npm install && cd ../
+# cd ./backend && npm install && cd ../
